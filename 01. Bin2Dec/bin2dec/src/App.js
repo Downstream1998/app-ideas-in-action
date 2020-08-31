81c1d2eb18b5d1e816d640ffe60199f2090580af
Liu Zhongyu <seLiuZhongyu@outlook.com>
2020-08-31 14:43:02 +0800

    完成基础功能开发

--- 01. Bin2Dec/bin2dec/src/App.js
+++ 01. Bin2Dec/bin2dec/src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState(0)
+  const [value, setValue] = useState('')
+
+  const computeDecimal = (binaryNumber) => {
+    let result = 0
+    for (let i = binaryNumber.length - 1; i >= 0; i--) {
+      result += (binaryNumber[i] - '0') * Math.pow(2, (binaryNumber.length - i - 1))
+    }
+    return result
+  }
+
+  const isBinaryNumber = (binaryNumber) => {
+    const result = /^[0-1]{0,8}$/.test(binaryNumber)
+    console.log('test result: ', result)
+    return result
+  }
+
+  const handleTextChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const handleOnClick = (event) => {
+    event.preventDefault()
+    console.log('value = ', value)
+    if (value.trim() === '') {
+      return
+    }
+    if (!isBinaryNumber(value)) {
+      setResult(NaN.toString())
+      return
+    }
+    setResult(computeDecimal(value))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleOnClick}>
+        <label htmlFor="bin">Please input a binary number (max 8-digits): </label>
+        <input type="text" id="bin" name="bin" maxLength="8" onChange={handleTextChange} value={value} />
+        <button type="submit">submit</button>
+        <div>
+          <p>The result is: <span>{result}</span></p>
+        </div>
+      </form>
     </div>
   );
 }
